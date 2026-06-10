eea79d4cb8b07af9463a11f2deeba109496475f2
Pari <pariaggarwal2021@gmail.com>
2026-06-10 09:57:27 +0530

    day 6 toggleswitch component - all tests passing

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,35 @@
 import './ToggleSwitch.css';
 
-export const ToggleSwitch = () => {
-  return <div></div>;
+export const ToggleSwitch = ({
+  id = 'toggle-switch',
+  label,
+  checked,
+  defaultChecked,
+  onChange,
+  disabled,
+  ...props
+}) => {
+  return (
+    <div className="toggle-switch" {...props}>
+      <input
+        id={id}
+        type="checkbox"
+        className="sr-only"
+        checked={checked}
+        defaultChecked={defaultChecked}
+        onChange={onChange}
+        disabled={disabled}
+      />
+
+      <label htmlFor={id} className="switch-slider">
+        <span className="switch-thumb"></span>
+      </label>
+
+      {label && (
+        <label htmlFor={id} className="switch-label">
+          {label}
+        </label>
+      )}
+    </div>
+  );
 };
