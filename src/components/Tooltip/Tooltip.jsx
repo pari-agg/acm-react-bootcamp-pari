d57e6b0818426b91b38f7635554c27d0050eb9f2
Pari <pariaggarwal2021@gmail.com>
2026-06-14 23:23:39 +0530

    day 7 tooltip component - all tests passing

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
 import './Tooltip.css';
 
-export const Tooltip = () => {
-  return <div></div>;
+export const Tooltip = ({ content, children }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="tooltip-wrapper">
+      <div
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        onFocus={() => setVisible(true)}
+        onBlur={() => setVisible(false)}
+      >
+        {children}
+      </div>
+
+      {visible && (
+        <div
+          className="tooltip-content"
+          role="tooltip"
+        >
+          {content}
+        </div>
+      )}
+    </div>
+  );
 };
